e201835527636f6b79ad73f54952e2099a8bb33a
Abbigail Merrill <abbigail.merrill@gmail.com>
2023-12-14 00:36:10 -0500

    updated active states and more app functionality

--- advice-generator-app/js/app.js
+++ advice-generator-app/js/app.js
@@ -1,13 +1,17 @@
 const advice = jQuery(".advice");
 const button = jQuery(".circle");
+const adviceNum = jQuery("h3");
 
 function getAdvice(){
 fetch("https://api.adviceslip.com/advice").then(response => {
     return response.json();
 }).then(data => {
     const adviceObj = data.slip.advice;
+    const adviceId = data.slip.id;
     advice.empty();
     advice.html(adviceObj);
+    adviceNum.empty();
+    adviceNum.html("advice #" + adviceId);
 }).catch(error =>{
     console.log(error);
 });
